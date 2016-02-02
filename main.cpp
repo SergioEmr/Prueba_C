2fd495b72380039d4d663fd5a2944dc7de8505b6
Sergio Morales <sergio_emr@outlook.com>
2016-02-02 02:17:00 -0600

    Este es el archivo base de la prueba

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+class humano
+{
+public:
+	char nombre[20];
+	int edad;
+
+public:
+	int setedad();
+	void setnombre(char[]);
+};
+
+int humano::setedad(){
+	return edad;
+}
+
+void humano::setnombre(char nom[]){
+	cout<<nom<<endl;
+}
+
+int main()
+{
+
+	humano persona1;
+	strcpy(persona1.nombre,"jorge");
+    persona1.edad=20;
+    cout<<"la edad de "<<persona1.nombre <<" es: "<<persona1.edad<<endl;
+	cin.get();
+	return 0;
+}
